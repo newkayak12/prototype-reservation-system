9a24637f808f8361c7f83930f2addfe4b1e05bf7
newkayak12 <newkayak12@gmail.com>
2025-05-19 22:06:06 +0900

    vault backup: 2025-05-19 22:06:06

--- init.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `prototype-reservation`;
-CREATE USER IF NOT EXISTS 'temporary'@'%' IDENTIFIED BY 'temporary';
-GRANT ALL PRIVILEGES ON `prototype-reservation`.* TO 'temporary'@'%';
-FLUSH PRIVILEGES;
