c408fbd417076f415878510564d5e490e3e0eeaf
newkayak12 <newkayak12@gmail.com>
2025-05-26 23:03:37 +0900

    feat: adapter

--- adapter-module/src/main/kotlin/com/reservation/config/mvc/WebConfig.kt
+++ adapter-module/src/main/kotlin/com/reservation/config/mvc/WebConfig.kt
@@ -1,12 +1,20 @@
 package com.reservation.config.mvc
 
 import com.reservation.config.i18n.HttpHeaderLocaleResolver
+import org.springframework.context.MessageSource
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean
 import org.springframework.web.servlet.LocaleResolver
 
 @Configuration
 internal class WebConfig {
     @Bean
     fun localeResolver(): LocaleResolver = HttpHeaderLocaleResolver()
+
+    @Bean
+    fun LocalValidatorFactoryBean(messageSource: MessageSource): LocalValidatorFactoryBean =
+        LocalValidatorFactoryBean().apply {
+            setValidationMessageSource(messageSource)
+        }
 }
