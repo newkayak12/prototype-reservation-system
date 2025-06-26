ca3b98cd7552f4d1f864423b719141bd35f439a6
newkayak12 <newkayak12@gmail.com>
2025-06-26 16:26:12 +0900

    chore: typo flyway migration 내용 요류

--- adapter-module/src/main/resources/db/migration/V1_3__add_use_uuid_to_user_change_history.sql
+++ adapter-module/src/main/resources/db/migration/V1_3__add_use_uuid_to_user_change_history.sql
@@ -1,4 +1,3 @@
 ALTER TABLE prototype_reservation.user_change_history ADD COLUMN user_uuid VARCHAR(128) comment '사용자 식별키' AFTER id;
 ALTER TABLE prototype_reservation.user_change_history DROP COLUMN fail_count;
 ALTER TABLE prototype_reservation.user_change_history DROP COLUMN locked_datetime;
-ALTER TABLE prototype_reservation.user_change_history DROP COLUMN user_status;
