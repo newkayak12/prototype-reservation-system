606c22b84144e8665f3eacc60abfc589f5a126cd
newkayak12 <newkayak12@gmail.com>
2025-06-26 15:28:59 +0900

    fix: entity, table의 컬럼 조정

--- adapter-module/src/main/kotlin/com/reservation/persistence/user/entity/UserChangeHistoryEntity.kt
+++ adapter-module/src/main/kotlin/com/reservation/persistence/user/entity/UserChangeHistoryEntity.kt
@@ -11,7 +11,6 @@ import jakarta.persistence.Enumerated
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import org.hibernate.annotations.Comment
-import java.time.LocalDateTime
 
 @Table(
     catalog = "prototype_reservation",
@@ -19,6 +18,7 @@ import java.time.LocalDateTime
 )
 @Entity
 class UserChangeHistoryEntity(
+    uuid: String,
     email: String,
     nickname: String,
     mobile: String,
@@ -30,6 +30,15 @@ class UserChangeHistoryEntity(
     @Column(name = "id", columnDefinition = "VARCHAR(128)", nullable = false, updatable = false)
     @Comment("식별키")
     val id: String? = null
+
+    @Column(
+        name = "user_uuid",
+        columnDefinition = "VARCHAR(128)",
+        nullable = false,
+        updatable = false,
+    )
+    @Comment("식별키")
+    val userUuid: String = uuid
 
     @Column(name = "user_id", columnDefinition = "VARCHAR(128)")
     @Comment("식별키")
@@ -54,16 +63,6 @@ class UserChangeHistoryEntity(
     @Enumerated(value = EnumType.STRING)
     @Comment("역할 (ROOT, SELLER, USER)")
     val role: Role = role
-
-    @Column(name = "fail_count", columnDefinition = "TINYINT")
-    @Comment("실패 횟수")
-    var failCount: Int = 0
-        protected set
-
-    @Column(name = "locked_datetime", columnDefinition = "DATETIME")
-    @Comment("로그인 실패 카운트")
-    var lockedDatetime: LocalDateTime? = null
-        protected set
 
     @Embedded
     var auditDateTime = AuditDateTime()
---
+++ adapter-module/src/main/resources/db/migration/V1__3__add_use_uuid_to_user_change_history.sql
@@ -0,0 +1,4 @@
+ALTER TABLE prototype_reservation.user_change_history ADD COLUMN user_uuid VARCHAR(128) comment '사용자 식별키' AFTER id;
+ALTER TABLE prototype_reservation.user_change_history DROP COLUMN fail_count;
+ALTER TABLE prototype_reservation.user_change_history DROP COLUMN locked_datetime;
+ALTER TABLE prototype_reservation.user_change_history DROP COLUMN user_status;
