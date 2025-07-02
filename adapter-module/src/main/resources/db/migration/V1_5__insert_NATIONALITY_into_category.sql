5445fd85c0a2411084be0cefbb3ca5bd4432bbbb
newkayak12 <newkayak12@gmail.com>
2025-07-02 10:44:23 +0900

    chore: add flyway sql script-category

---
+++ adapter-module/src/main/resources/db/migration/V1_5__insert_NATIONALITY_into_category.sql
@@ -0,0 +1,8 @@
+INSERT INTO prototype_reservation.`nationality`
+    (title, category_type)
+VALUES ('한식', 'NATIONALITY'),
+       ('양식', 'NATIONALITY'),
+       ('중식', 'NATIONALITY'),
+       ('일식', 'NATIONALITY'),
+       ('아시아', 'NATIONALITY'),
+       ('중남미', 'NATIONALITY')
