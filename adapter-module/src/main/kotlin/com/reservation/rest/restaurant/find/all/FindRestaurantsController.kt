3b5ab0f0104117c03cfaa350c8ffa44a033e8986
newkayak12 <newkayak12@gmail.com>
2025-08-14 14:19:40 +0900

    feat: FindRestaurants RestAdapter 작성
    1. PageResponse 작성
    2. Response 변환 과정 추가
    3. Adapter에 대한 테스트 추가

---
+++ adapter-module/src/main/kotlin/com/reservation/rest/restaurant/find/all/FindRestaurantsController.kt
@@ -0,0 +1,22 @@
+package com.reservation.rest.restaurant.find.all
+
+import com.reservation.rest.common.response.PageResponse
+import com.reservation.rest.restaurant.RestaurantUrl
+import com.reservation.rest.restaurant.request.FindRestaurantsRequest
+import com.reservation.rest.restaurant.response.FindRestaurantsResponses
+import com.reservation.rest.restaurant.response.FindRestaurantsResponses.FindRestaurantsResponse
+import com.reservation.restaurant.port.input.FindRestaurantsUseCase
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class FindRestaurantsController(
+    private val findRestaurantsUseCase: FindRestaurantsUseCase,
+) {
+    @GetMapping(RestaurantUrl.FIND_A_RESTAURANTS)
+    fun findRestaurants(request: FindRestaurantsRequest): PageResponse<FindRestaurantsResponse> {
+        val result = findRestaurantsUseCase.execute(request.toQuery())
+        val transformed = FindRestaurantsResponses.from(result)
+        return PageResponse.ok(transformed.list, transformed.hasNext)
+    }
+}
---
+++ adapter-module/src/main/kotlin/com/reservation/rest/restaurant/request/FindRestaurantsRequest.kt
@@ -0,0 +1,22 @@
+package com.reservation.rest.restaurant.request
+
+import com.reservation.restaurant.port.input.query.request.FindRestaurantsQueryRequest
+
+data class FindRestaurantsRequest(
+    val identifierFrom: String = "",
+    val size: Long = 10,
+    val searchText: String = "",
+    val tags: List<Long> = listOf(),
+    val nationalities: List<Long> = listOf(),
+    val cuisines: List<Long> = listOf(),
+) {
+    fun toQuery() =
+        FindRestaurantsQueryRequest(
+            identifierFrom,
+            size,
+            searchText,
+            tags,
+            nationalities,
+            cuisines,
+        )
+}
