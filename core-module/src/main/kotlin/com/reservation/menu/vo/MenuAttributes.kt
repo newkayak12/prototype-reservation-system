728dd633736037693f0b728c5b53c87020fc6142
newkayak12 <newkayak12@gmail.com>
2025-08-19 21:30:20 +0900

    feat(domain): domain 생성 1. DomainEntity 생성 2. VO 생성

---
+++ core-module/src/main/kotlin/com/reservation/menu/vo/MenuAttributes.kt
@@ -0,0 +1,7 @@
+package com.reservation.menu.vo
+
+data class MenuAttributes(
+    val isRepresentative: Boolean = false,
+    val isRecommended: Boolean = false,
+    val isVisible: Boolean = false,
+)
---
+++ core-module/src/main/kotlin/com/reservation/menu/vo/MenuDescription.kt
@@ -0,0 +1,6 @@
+package com.reservation.menu.vo
+
+data class MenuDescription(
+    val title: String,
+    val description: String,
+)
