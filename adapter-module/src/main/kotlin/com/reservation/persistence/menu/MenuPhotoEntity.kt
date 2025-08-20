105afd77bb45eeaa6a816dae81c3dfc091ddb104
newkayak12 <newkayak12@gmail.com>
2025-08-20 22:31:29 +0900

    feat(persistence): jpa entity 생성

---
+++ adapter-module/src/main/kotlin/com/reservation/persistence/menu/MenuPhotoEntity.kt
@@ -0,0 +1,32 @@
+package com.reservation.persistence.menu
+
+import com.reservation.persistence.common.TimeBasedPrimaryKey
+import jakarta.persistence.Column
+import jakarta.persistence.ConstraintMode.NO_CONSTRAINT
+import jakarta.persistence.Entity
+import jakarta.persistence.ForeignKey
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import org.hibernate.annotations.Comment
+
+@Table(
+    catalog = "prototype_reservation",
+    name = "restaurant_photo",
+    indexes = [],
+)
+@Entity
+class MenuPhotoEntity(
+    @ManyToOne
+    @JoinColumn(
+        name = "menu_id",
+        nullable = false,
+        updatable = false,
+        foreignKey = ForeignKey(NO_CONSTRAINT),
+    )
+    @Comment("메뉴 식별키")
+    val menu: MenuEntity,
+    @Column(name = "url", nullable = false, updatable = false)
+    @Comment("URL")
+    val url: String,
+) : TimeBasedPrimaryKey()
