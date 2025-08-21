b1f310cc0da4b610a7f244913a71495dde9b1840
newkayak12 <newkayak12@gmail.com>
2025-08-21 19:34:40 +0900

    feat(persistence): save as JPA

---
+++ adapter-module/src/main/kotlin/com/reservation/persistence/menu/repository/jpa/MenuJpaRepository.kt
@@ -0,0 +1,8 @@
+package com.reservation.persistence.menu.repository.jpa
+
+import com.reservation.persistence.menu.entity.MenuEntity
+import org.springframework.data.repository.CrudRepository
+
+interface MenuJpaRepository : CrudRepository<MenuEntity, String> {
+    fun save(entity: MenuEntity): MenuEntity
+}
