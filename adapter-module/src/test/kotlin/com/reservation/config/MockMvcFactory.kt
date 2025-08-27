5aa7122e8d6e15a6498379f4ffd69232396bbc90
newkayak12 <newkayak12@gmail.com>
2025-08-27 10:22:19 +0900

    test(adapter): @MockMVC가 아닌 Mock 테스트

--- adapter-module/src/test/kotlin/com/reservation/config/MockMvcFactory.kt
+++ adapter-module/src/test/kotlin/com/reservation/config/MockMvcFactory.kt
@@ -3,7 +3,10 @@ package com.reservation.config
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.reservation.config.mvc.RestControllerExceptionHandler
 import jakarta.validation.Validation
+import org.springframework.core.env.ConfigurableEnvironment
+import org.springframework.core.env.StandardEnvironment
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.restdocs.ManualRestDocumentation
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
@@ -20,6 +23,13 @@ object MockMvcFactory {
     private val validator = Validation.buildDefaultValidatorFactory().validator
     private val converter = MappingJackson2HttpMessageConverter(jacksonObjectMapper)
     private val springValidator = SpringValidatorAdapter(validator)
+    private val environment: ConfigurableEnvironment =
+        StandardEnvironment().also {
+            it.setActiveProfiles("test")
+        }
+    private val restControllerExceptionHandler =
+        RestControllerExceptionHandler()
+            .also { it.setEnvironment(environment) }
 
     val objectMapper
         get() = jacksonObjectMapper
@@ -28,13 +38,18 @@ object MockMvcFactory {
         MockMvcBuilders.standaloneSetup(controller)
             .setMessageConverters(converter)
             .setValidator(springValidator)
+            .setControllerAdvice(restControllerExceptionHandler)
 
     fun <T> buildMockMvc(controller: T): MockMvc = getStandAloneSetup(controller).build()
 
     fun <T> buildMockMvc(
         controller: T,
         restDocumentation: ManualRestDocumentation,
-    ) = getStandAloneSetup(controller)
-        .apply { (MockMvcRestDocumentation.documentationConfiguration(restDocumentation)) }
-        .build()
+    ): MockMvc {
+        return getStandAloneSetup(controller)
+            .apply<org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder>(
+                MockMvcRestDocumentation.documentationConfiguration(restDocumentation),
+            )
+            .build()
+    }
 }
