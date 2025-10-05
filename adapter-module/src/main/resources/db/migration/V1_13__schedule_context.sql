8d7707f6dbe7847338462a1a4634f7416d55b47c
newkayak12 <newkayak12@gmail.com>
2025-10-05 16:01:30 +0900

    fix(repository): 스케줄 컬럼명 및 테이블 추가 반영
    - schedule_context 마이그레이션 파일에서 컬럼명 변경
      - startTime start_time
      endTime -> end_time
    - 새로운 테이블 추가: prototype_reservation.table
      - 컬럼: id, restaurant_id, table_number, table_size
      - 기본키: id
      - 문자셋: utf8mb4, 콜레이션: utf8mb4_general_ci
      - 엔진: InnoDB
    
    ISSUE-1234

--- adapter-module/src/main/resources/db/migration/V1_13__schedule_context.sql
+++ adapter-module/src/main/resources/db/migration/V1_13__schedule_context.sql
@@ -18,8 +18,8 @@ CREATE TABLE prototype_reservation.time_span
     id            VARCHAR(128) COMMENT '식별키',
     restaurant_id VARCHAR(128)                                                                               NOT NULL COMMENT '음식점 식별키',
     day           ENUM ('MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY','SUNDAY') COMMENT '요일' NOT NULL,
-    startTime     TIME                                                                                       NOT NULL COMMENT '설명',
-    endTime       TIME                                                                                       NOT NULL COMMENT '설명',
+    start_time     TIME                                                                                       NOT NULL COMMENT '설명',
+    end_time       TIME                                                                                       NOT NULL COMMENT '설명',
     PRIMARY KEY (id),
     INDEX index_restaurant_id (restaurant_id)
 ) ENGINE = innodb
@@ -37,3 +37,13 @@ CREATE TABLE prototype_reservation.holiday
   DEFAULT CHARACTER SET 'utf8mb4'
   COLLATE 'utf8mb4_general_ci';
 
+CREATE TABLE prototype_reservation.table
+(
+    id            VARCHAR(128) COMMENT '식별키',
+    restaurant_id VARCHAR(128) NOT NULL COMMENT '음식점 식별키',
+    table_number INT COMMENT '테이블 번호',
+    table_size INT COMMENT '좌석 수',
+    PRIMARY KEY (id)
+) ENGINE = innodb
+  DEFAULT CHARACTER SET 'utf8mb4'
+  COLLATE 'utf8mb4_general_ci';
